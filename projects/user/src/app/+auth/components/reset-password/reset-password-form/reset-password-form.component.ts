5f00922ded06aac36ee9595045cf4a351e55114d
Akanalanka Gajasinghe <angajasinghe@outlook.com>
2020-12-28 08:57:04 +0530

    subject error completed

--- projects/user/src/app/+auth/components/reset-password/reset-password-form/reset-password-form.component.ts
+++ projects/user/src/app/+auth/components/reset-password/reset-password-form/reset-password-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, Validators} from '@angular/forms';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-reset-password-form',
@@ -11,9 +12,12 @@ export class ResetPasswordFormComponent implements OnInit {
   showDetails = true;
   hide = true;
   hide2 = true;
-  constructor() { }
+
+  constructor(private activatedRouter: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const authToken = this.activatedRouter.snapshot.queryParams.auth_token;
+    console.log(authToken);
   }
   onStrengthChanged(strength: number): void {
     console.log('password strength = ', strength);
