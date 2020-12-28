6fb59d1ce2d091a5ce430330f39442526722de87
TharindaNimnajith <tharindarajapakshe@gmail.com>
2020-12-28 16:05:52 +0530

    book

---
+++ projects/user/src/app/private/@ui/user-content/book/page/user-book/user-book.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import {Store} from '@ngrx/store';
+import * as fromApp from '../../../../../../app.reducer';
+import {BOOK_DATA_REQUEST} from '../../store/book.action';
+
+@Component({
+  selector: 'app-user-book',
+  templateUrl: './user-book.component.html',
+  styleUrls: ['./user-book.component.scss']
+})
+export class UserBookComponent implements OnInit {
+
+  constructor(private store: Store<fromApp.AppState>) { }
+
+  ngOnInit(): void {
+    this.store.dispatch(BOOK_DATA_REQUEST());
+  }
+
+}
