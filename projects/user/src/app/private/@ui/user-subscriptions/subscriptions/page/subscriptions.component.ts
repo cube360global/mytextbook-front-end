9c71db119fd80de8be2b12affd144e1d5b164a85
TharindaNimnajith <tharindarajapakshe@gmail.com>
2020-12-29 10:40:55 +0530

    subscriptions

---
+++ projects/user/src/app/private/@ui/user-subscriptions/subscriptions/page/subscriptions.component.ts
@@ -0,0 +1,14 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-user-subscriptions',
+  templateUrl: './subscriptions.component.html',
+  styleUrls: ['./subscriptions.component.scss']
+})
+export class SubscriptionsComponent implements OnInit {
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+}
