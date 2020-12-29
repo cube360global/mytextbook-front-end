07aa05d4681d9d8b8d7ae32f6b5da0796aa9dd87
TharindaNimnajith <tharindarajapakshe@gmail.com>
2020-12-29 16:43:14 +0530

    videos

--- projects/user/src/app/private/@ui/user-details/user-subscriptions/components/book-content/video-list/video-item/video-item.component.ts
+++ projects/user/src/app/private/@ui/user-details/user-subscriptions/components/book-content/video-list/video-item/video-item.component.ts
@@ -1,6 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {ContentModel} from '../../../../../../../../../../../administration/src/app/@core/interfaces/api/ContentModel';
-import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-video-item',
@@ -8,15 +7,11 @@ import {DomSanitizer} from '@angular/platform-browser';
   styleUrls: ['./video-item.component.scss']
 })
 export class VideoItemComponent implements OnInit {
-  contentData = {} as ContentModel;
-  videoUrl: any;
+  @Input() video = {} as ContentModel;
 
-  constructor(private sanitizer: DomSanitizer) {
+  constructor() {
   }
 
   ngOnInit(): void {
-    this.contentData = window.history.state;
-    this.videoUrl = this.sanitizer
-      .bypassSecurityTrustResourceUrl(`https://player.vimeo.com/video/${this.contentData.contentURL}`);
   }
 }
