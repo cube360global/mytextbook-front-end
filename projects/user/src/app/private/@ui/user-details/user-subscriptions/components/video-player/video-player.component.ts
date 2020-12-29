1a992212b16f283a55db77c1bbf495753b1b1bc3
TharindaNimnajith <tharindarajapakshe@gmail.com>
2020-12-29 16:52:54 +0530

    videos

--- projects/user/src/app/private/@ui/user-details/user-subscriptions/components/video-player/video-player.component.ts
+++ projects/user/src/app/private/@ui/user-details/user-subscriptions/components/video-player/video-player.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {ContentModel} from '../../../../../../@core/interfaces/api/ContentModel';
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-video-player',
@@ -6,9 +8,15 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./video-player.component.scss']
 })
 export class VideoPlayerComponent implements OnInit {
-  constructor() {
+  contentData = {} as ContentModel;
+  videoUrl: any;
+
+  constructor(private sanitizer: DomSanitizer) {
   }
 
   ngOnInit(): void {
+    this.contentData = window.history.state;
+    this.videoUrl = this.sanitizer
+      .bypassSecurityTrustResourceUrl(`https://player.vimeo.com/video/${this.contentData.contentURL}`);
   }
 }
