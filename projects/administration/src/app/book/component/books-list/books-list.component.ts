f32323d76bcc6a1166a6244f469e507515473093
aroshaj <acjayathilake@gmail.com>
2021-01-07 21:18:18 +0530

    skeleton updated

--- projects/administration/src/app/book/component/books-list/books-list.component.ts
+++ projects/administration/src/app/book/component/books-list/books-list.component.ts
@@ -5,6 +5,7 @@ import {Store} from '@ngrx/store';
 import * as fromApp from '../../../app.reducer';
 import {BookApiService} from '../../shared/services/book-api.service';
 import {BookModel} from '../../../@core/interfaces/api/BookModel';
+import {EditBookComponent} from '../edit-book/edit-book.component';
 
 @Component({
   selector: 'app-books-list',
@@ -44,9 +45,9 @@ export class BooksListComponent implements OnInit {
   }
 
   openBookEditViewDialog(book: BookModel): void {
-    // this.dialog.open(EditBookComponent, {
-    //   width: '100%',
-    //   data: book
-    // });
+    this.dialog.open(EditBookComponent, {
+      width: '100%',
+      data: book
+    });
   }
 }
--- projects/administration/src/app/book/component/edit-book/edit-book.component.ts
+++ projects/administration/src/app/book/component/edit-book/edit-book.component.ts
@@ -1,6 +1,7 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {BookModel} from '../../../@core/interfaces/api/BookModel';
 import {MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-edit-book',
@@ -9,11 +10,14 @@ import {MAT_DIALOG_DATA} from '@angular/material/dialog';
 })
 export class EditBookComponent implements OnInit {
 
+  editSubjectForm = {} as FormGroup;
   constructor(@Inject(MAT_DIALOG_DATA) public bookModel: BookModel) {
   }
 
   ngOnInit(): void {
-    console.log(this.bookModel);
+    // console.log(this.bookModel);
+    // // this.editSubjectForm = new FormGroup(
+    // // })
   }
 
 }
