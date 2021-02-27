8054692809900b9add049bf0f1578268ca1bc32b
ANgajasinghe <Akalanka Nayanajith@DESKTOP-TERE1H0>
2021-02-27 17:04:45 +0530

    update server.js - 01

--- server.js
+++ server.js
@@ -5,21 +5,21 @@ const app = express()
 app.listen(process.env.PORT || 8080);
 
 
-app.use(express.static(__dirname + '/user/'));
-app.use(express.static(__dirname + '/administration/'));
+app.use(express.static(__dirname + '/dist/user/'));
+app.use(express.static(__dirname + '/dist/administration/'));
 
 app.get('/health-check', (req, res) => {
   return res.send('online');
 });
 
 app.get('/administration', function (req, res) {
-  res.sendFile(path.join(__dirname + '/administration/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/administration/index.html'));
 });
 
 app.get('/administration/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/administration/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/administration/index.html'));
 });
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/user/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/user/index.html'));
 });
